d63e262fb21f1e7efe40ab4d8a6086877c3c9d2c
Chaitanya <chaitu.vsh@gmail.com>
2023-03-17 21:00:54 +0530

    [JAVASCRIPT] Adding menu functionality

--- 10_News_LandingPage/js/script.js
+++ 10_News_LandingPage/js/script.js
@@ -1,1 +1,24 @@
-console.log("YAYYY");
+const menuOpenButton = document.getElementById("menuOpenButton")
+const menuCloseButton = document.getElementById("menuCloseButton")
+
+const menu = document.getElementById("navUl");
+
+const showMenu = () => {
+    menu.style.right = 0;
+    menu.style.display = "block"
+    menuCloseButton.style.display = "block"
+}
+menuOpenButton.addEventListener("click", showMenu);
+
+
+const hideMenu = () => {
+    menu.style.display = "none";
+    menu.style.right = "70%"
+    menuCloseButton.style.display = "none"
+}
+menuCloseButton.addEventListener("click", hideMenu);
+
+
+// window.onresize = () => {
+//     hideMenu();
+// };
